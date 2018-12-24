0da2f4f910bc1963ea6355d11c43ff3df71486ba
FROGbots-4634 <teamfrogbots@gmail.com>
2018-12-24 15:45:37 -0500

    get rid of fab in lynx control activity

--- OpModeTuner/src/main/java/net/frogbots/ftcopmodetuner/ui/activity/hubtoolkit/LynxModuleControlActivity.java
+++ OpModeTuner/src/main/java/net/frogbots/ftcopmodetuner/ui/activity/hubtoolkit/LynxModuleControlActivity.java
@@ -63,18 +63,6 @@ public class LynxModuleControlActivity extends AppCompatActivity
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
